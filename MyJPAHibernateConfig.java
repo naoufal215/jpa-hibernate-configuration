6ff0ee65dcbb183bb0b945143ac187b58425971e
naoufal <naoufal.berouain@gmail.com>
2017-04-19 10:13:11 +0000

    jpa hibernate spring mvc configuration

---
+++ MyJPAHibernateConfig.java
@@ -0,0 +1,82 @@
+package surtymar.net.configuration;
+
+import java.util.Properties;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+@ComponentScan({ "surtymar.net.configuration" })
+@PropertySource({ "classpath:application.properties" })
+public class MyJPAHibernateConfig {
+
+	// *************************************
+	@Autowired
+	private Environment environment;
+
+	// *************************************
+	@Bean
+	public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+		LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+		em.setDataSource(dataSource());
+		em.setPackagesToScan(new String[] { "surtymar.net.entities", "surtymar.net.dao" });
+
+		JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+		em.setJpaVendorAdapter(vendorAdapter);
+		em.setJpaProperties(hibernateProperties());
+
+		return em;
+	}
+
+	@Bean
+	public DataSource dataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName(environment.getRequiredProperty("datasource.driver"));
+		dataSource.setUrl(environment.getRequiredProperty("spring.datasource.url"));
+		dataSource.setUsername(environment.getRequiredProperty("spring.datasource.username"));
+		dataSource.setPassword(environment.getRequiredProperty("spring.datasource.username"));
+		return dataSource;
+	}
+
+	@Bean
+	public PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
+		JpaTransactionManager transactionManager = new JpaTransactionManager();
+		transactionManager.setEntityManagerFactory(emf);
+
+		return transactionManager;
+	}
+
+	@Bean
+	public PersistenceExceptionTranslationPostProcessor exceptionTranslation() {
+		return new PersistenceExceptionTranslationPostProcessor();
+	}
+
+	private Properties hibernateProperties() {
+		Properties properties = new Properties();
+		properties.put("hibernate.hbm2ddl.auto", environment.getRequiredProperty("spring.jpa.hibernate.ddl-auto"));
+		properties.put("hibernate.dialect", environment.getRequiredProperty("spring.jpa.properties.hibernate.dialect"));
+		properties.put("hibernate.show_sql", environment.getRequiredProperty("spring.jpa.show-sql"));
+		properties.put("hibernate.format_sql",
+								environment.getRequiredProperty("spring.jpa.properties.hibernate.format_sql"));
+		properties.put("hibernate.naming.physical-strategy",
+								environment.getRequiredProperty("spring.jpa.hibernate.naming.physical-strategy"));
+		return properties;
+	}
+
+}
---
+++ MySpringMVCConfig.java
@@ -0,0 +1,51 @@
+package surtymar.net.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceView;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+
+@EnableWebMvc
+@Configuration
+@ComponentScan(basePackages = { "surtymar.net.services" })
+public class MySpringMVCConfig extends WebMvcConfigurerAdapter {
+	
+	
+	
+	    @Override
+	    public void addViewControllers(ViewControllerRegistry registry) {
+	        registry.addViewController("/").setViewName("index");
+	    }
+	
+	   @Override
+	    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+	        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+	        registry.addResourceHandler("/css/**").addResourceLocations("/static/css/");
+	        registry.addResourceHandler("/images/**").addResourceLocations("/static/images/");
+	        registry.addResourceHandler("/js/**").addResourceLocations("/static/js/");
+	    }
+	   	
+	   @Bean
+	    public InternalResourceViewResolver viewResolver(){
+	        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+	        viewResolver.setSuffix(".html");
+	        viewResolver.setViewClass(InternalResourceView.class);
+	        return viewResolver;
+	    }
+	   
+	    @Override
+	    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+	        configurer.enable();
+	    }
+	   
+	
+	    
+
+}
